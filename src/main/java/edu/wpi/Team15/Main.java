25711b88c2736f9502c44efd23be716f96271c92
charkw <cckneisslwilliam@wpi.edu>
2022-03-18 15:49:09 -0400

    Added out with my name

--- src/main/java/edu/wpi/Team15/Main.java
+++ src/main/java/edu/wpi/Team15/Main.java
@@ -5,6 +5,7 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Hello World!");
     System.out.println("Jason Odell");
+    System.out.println("Charlie");
     App.launch(App.class, args);
   }
 }
