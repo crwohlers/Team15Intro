43ffa08a5cbdf1214d08d9a0b17e3e984b4a9544
hg <32725385+HexCodeZero@users.noreply.github.com>
2022-03-18 15:53:00 -0400

    added name

--- src/main/java/edu/wpi/Team15/Main.java
+++ src/main/java/edu/wpi/Team15/Main.java
@@ -5,6 +5,7 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Hello World!");
     System.out.println("Jason Odell");
+    System.out.println("Hasan Gandor");
     App.launch(App.class, args);
   }
 }
